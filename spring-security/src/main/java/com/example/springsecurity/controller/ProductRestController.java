142a2d7f3c61d5b78f52fcf4508b82a6f7ab55e1
loic <loic.gstrn@gmail.com>
2024-03-18 14:16:26 +0100

    spring security correction

---
+++ spring-security/src/main/java/com/example/springsecurity/controller/ProductRestController.java
@@ -0,0 +1,47 @@
+package com.example.springsecurity.controller;
+
+import com.example.springsecurity.model.Product;
+import com.example.springsecurity.repository.ProductRepository;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("api-v1")
+public class ProductRestController {
+
+
+    private final ProductRepository productRepository;
+
+    public ProductRestController(ProductRepository productRepository) {
+        this.productRepository = productRepository;
+    }
+
+    @GetMapping("/product/{id}") // http://localhost:8080/api-v1/product/*
+    public Optional<Product> get(@PathVariable("id") Long id){
+        return productRepository.findById(id);
+    }
+
+    @GetMapping("/products") // http://localhost:8080/api-v1/products
+    public List<Product> get(){
+        return (List<Product>) productRepository.findAll();
+    }
+
+    @PostMapping("/product") // http://localhost:8080/api-v1/product
+    public void post(@RequestBody Product product){
+        productRepository.save(product);
+    }
+
+    @DeleteMapping("/product/{id}") // http://localhost:8080/api-v1/product/*
+    public void delete(@PathVariable("id") Long id){
+        productRepository.deleteProductById(id);
+    }
+
+    @PutMapping("product/{id}") // http://localhost:8080/api-v1/product/*
+    public void update(@PathVariable("id") Long id, @RequestBody Product productUpdate){
+
+
+    }
+
+}
---
+++ spring-security/src/main/java/com/example/springsecurity/model/Product.java
@@ -0,0 +1,26 @@
+package com.example.springsecurity.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+
+@Entity(name = "products")
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class Product {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private Double price;
+
+}
