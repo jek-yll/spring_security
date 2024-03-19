577b23e24bfe52261cf52a32bef345a36bc4f525
loic <loic.gstrn@gmail.com>
2024-03-19 16:50:35 +0100

    tp bank

---
+++ spring_security_tp1/src/main/java/com/example/spring_security_tp1/SpringSecurityTp1Application.java
@@ -0,0 +1,13 @@
+package com.example.spring_security_tp1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurityTp1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSecurityTp1Application.class, args);
+	}
+
+}
---
+++ spring_security_tp1/src/test/java/com/example/spring_security_tp1/SpringSecurityTp1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.spring_security_tp1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurityTp1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
